11282c439cf6920c6520cd9270719661dce16632
ramsalo1981 <rami.salloum@hotmail.com>
2023-10-13 14:28:25 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CAFinalizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // N: number with ,00 with space between 3numbers <=> 999 999,89
+            // N0: number without ,00 with space between 3numbers <=> 999 999
+            MakeSomGarbage();
+            Console.WriteLine($"Memory used before Collecion: {GC.GetTotalMemory(false):N0}");
+            MakeSomGarbage();
+            Console.WriteLine($"Memory used after collection: {GC.GetTotalMemory(true):N}");
+        }
+        static void MakeSomGarbage()
+        {
+            Version v;
+            for (int i = 0; i < 1000; i++)
+            {
+                v = new Version();
+            }
+        }
+    }
+
+    
+}
